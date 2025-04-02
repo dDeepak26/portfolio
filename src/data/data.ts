5693ee3a8184bff15c26205c2f7fe1ee4387fa23
dDeepak26 <deepakdurgam26@gmail.com>
2025-04-02 11:17:49 +0530

    chore: data

--- src/data/data.ts
+++ src/data/data.ts
@@ -2,19 +2,32 @@ export const aboutData = {
   name: "Deepak Durgam",
   bio: "Motivated fresher with a passion for web and mobile development. Eager to grow as a Full Stack Developer, adapt to challenges, and continuously improve. Skilled in problem-solving and teamwork, ready to contribute to dynamic teams and modern frameworks.",
   resumeLink:
-    "https://drive.google.com/file/d/1hkNYqkFQIyEZGHKOffR-f2AR0Y1q8kXm/view?usp=sharing",
+    "https://drive.google.com/file/d/17VSP97XdV4kT8ByVx_yjvmjHnMuwcfHG/view?usp=sharing",
 };
 
 export const projectData = [
   {
+    projectName: "CodePath",
+    techStack: {
+      1: "React",
+      2: "Tailwind CSS",
+      3: "Firebase",
+    },
+    description:
+      "Developed a full-stack coding platform similar to LeetCode, enabling users to solve JavaScript coding problems. Designed an intuitive UI with React, Tailwind CSS, and shadcnUI, enhancing user engagement by 25%. Implemented a dynamic code execution and submission system to validate solutions against test cases. Integrated Clerk for secure authentication and account management.",
+    firstButton: "GitHub",
+    firstButtonLink: "https://github.com/dDeepak26/CodePath.git",
+    secondButton: "Live",
+    secondButtonLink: "https://codepath.netlify.app/",
+  },
+  {
     projectName: "NetflixGPT",
     techStack: {
-      1: "React.js",
+      1: "React",
       2: "Tailwind CSS",
       3: "Redux",
       4: "Firebase",
     },
-    image: "Netflix.png",
     description:
       "The NetflixGPT project is a React-based app that integrates Redux, Tailwind CSS, Firebase, TMDB API, and Gemini API. It features movie rendering from TMDB, enabling users to view detailed information about each title. Firebase handles authentication with Google or email, while Gemini API powers a search function for movies. Users can switch between five languages for a customized experience. The project is hosted on Vercel, providing quick deployment and accessibility across devices.",
     firstButton: "GitHub",
@@ -23,27 +36,11 @@ export const projectData = [
     secondButtonLink: "https://netflix-gpt-ddeepak26s-projects.vercel.app/",
   },
   {
-    projectName: "AI Expense Tracker",
-    techStack: {
-      1: "Next.js",
-      2: "React.js",
-      3: "Tailwind CSS",
-      4: "shadcn UI",
-      5: "PostgreSQL",
-    },
-    image: "ai-expense-tracker.png",
-    description:
-      "The AI Expense Tracker is a Next.js and React app that uses TypeScript, Tailwind CSS, and Gemini API for intelligent financial tracking. It includes Drizzle, Neon, and PostgreSQL for secure data storage, allowing users to manage budgets, expenses, and income. Users can view financial summaries through dynamic graphs and receive AI-driven financial advice based on their data. Authentication is supported via Google or email and password. The app combines user-friendly UI with advanced analytics, offering measurable insights to improve financial decision-making.",
-    firstButton: "GitHub",
-    firstButtonLink: "https://github.com/dDeepak26/ai-expense-tracker.git",
-  },
-  {
     projectName: "Pet Buddy",
     techStack: {
       1: "React Native",
       2: "Firebase",
     },
-    image: "Ekart.png",
     description:
       "Pet Buddy is a React Native app using Firebase that helps users find pets for adoption. It includes Google authentication and stores pet data, allowing users to browse and filter pets by categories. Users can create a personalized favorite list by liking pets, connect with pet owners through in-app chat, and add new pets for adoption. Firebase handles real-time data sync and secure user interactions, making it a seamless platform for pet adoption enthusiasts.",
     firstButton: "GitHub",
@@ -67,10 +64,8 @@ export const skillsData: SkillsData[] = [
       HTML: "HTML.png",
       CSS: "CSS.png",
       JavaScript: "JavaScript.png",
-      "React JS": "React JS.png",
-      Bootstrap: "Bootstrap.png",
+      React: "React JS.png",
       "Tailwind CSS": "Tailwind CSS.png",
-      "Material UI": "Material UI.png",
     },
   },
   {
@@ -109,7 +104,7 @@ export const educationData = [
     universityImage: "MUMBAI.jpg",
     university: "Mumbai University",
     batch: "2023-25",
-    grade: "9.24 SGPA",
+    grade: "9.04 SGPA",
   },
   {
     degree: "Bachelor of Computer Applications",
